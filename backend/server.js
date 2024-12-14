0593be59cbf700bd6d7827494fdf44fe8ed751b8
abhishek tiwari <ht43280@gmail.com>
2024-12-14 17:08:08 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
@@ -10,14 +11,23 @@ import {app,server} from './socket/socket.js'
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+dotenv.config();
+
 app.use(express.json());  // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
-app.get("/", (req,res)=>{
-    res.send("hello wordl!");
-});
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
+
+//app.get("/", (req,res)=>{
+  //  res.send("hello wordl!");
+//});
 
 
 
